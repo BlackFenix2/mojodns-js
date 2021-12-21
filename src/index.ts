776440d43de3d159899fdbd1c7ef9a24d0c103ae
Ernie Francis <Devin_98@msn.com>
2021-12-21 11:36:43 -0500

    refactored to class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 interface Response {
   authoritativeAnswer: boolean;
@@ -55,52 +55,81 @@ interface TXTRecord extends Record {
   };
 }
 
-const DnsApi = axios.create({
-  baseURL: "https://api.mojodns.com/api/dns",
-  headers: {
-    Authorization: "2cf00d90-32d7-4a28-8ebd-d1cf3284e8ac",
-  },
-});
+export default class MojoDnsclient {
+  private readonly axiosClient: AxiosInstance;
 
-export const createClient = () => {};
-export const getDomainIP = async (domain: string) => {
-  const result: AxiosResponse<AResponse> = await DnsApi.get(`/${domain}/A`);
-
-  // check for status codes between 200 and 299
-
-  if (result.status < 200 || result.status > 299) {
-    throw new Error(result.statusText);
+  constructor(apiKey: string) {
+    this.axiosClient = axios.create({
+      baseURL: "https://api.mojodns.com/api/dns",
+      headers: {
+        Authorization: apiKey,
+      },
+    });
   }
 
-  // check if 200 response has empty answer record
-  if (result.data.responseCode !== "NO_ERROR") {
-    throw new Error(result.data.responseCode);
-  }
-  return result.data;
-};
+  public async getNS(domain: string): Promise<NSResponse> {
+    const result: AxiosResponse<NSResponse> = await this.axiosClient.get(
+      `/${domain}/NS`
+    );
 
-export const getDomainMX = async (domain: string) => {
-  const result: AxiosResponse<MXResponse> = await DnsApi.get(`/${domain}/MX`);
+    // check for status codes between 200 and 299
+    if (result.status < 200 || result.status > 299) {
+      throw new Error(result.statusText);
+    }
+    if (result.data.responseCode !== "NO_ERROR") {
+      throw new Error(result.data.responseCode);
+    }
+    return result.data;
+  }
 
-  // check for status codes between 200 and 299
-  if (result.status < 200 || result.status > 299) {
-    throw new Error(result.statusText);
-  }
-  if (result.data.responseCode !== "NO_ERROR") {
-    throw new Error(result.data.responseCode);
-  }
-  return result.data;
-};
+  public async getMX(domain: string): Promise<MXResponse> {
+    const result: AxiosResponse<MXResponse> = await this.axiosClient.get(
+      `/${domain}/MX`
+    );
 
-export const getDomainNS = async (domain: string) => {
-  const result: AxiosResponse<NSResponse> = await DnsApi.get(`/${domain}/NS`);
+    // check for status codes between 200 and 299
+    if (result.status < 200 || result.status > 299) {
+      throw new Error(result.statusText);
+    }
+    if (result.data.responseCode !== "NO_ERROR") {
+      throw new Error(result.data.responseCode);
+    }
+    return result.data;
+  }
 
-  // check for status codes between 200 and 299
-  if (result.status < 200 || result.status > 299) {
-    throw new Error(result.statusText);
+  public async getA(domain: string): Promise<AResponse> {
+    const result: AxiosResponse<AResponse> = await this.axiosClient.get(
+      `/${domain}/A`
+    );
+
+    // check for status codes between 200 and 299
+
+    if (result.status < 200 || result.status > 299) {
+      throw new Error(result.statusText);
+    }
+
+    // check if 200 response has empty answer record
+    if (result.data.responseCode !== "NO_ERROR") {
+      throw new Error(result.data.responseCode);
+    }
+    return result.data;
   }
-  if (result.data.responseCode !== "NO_ERROR") {
-    throw new Error(result.data.responseCode);
+
+  public async getTXT(domain: string): Promise<TXTResponse> {
+    const result: AxiosResponse<TXTResponse> = await this.axiosClient.get(
+      `/${domain}/TXT`
+    );
+
+    // check for status codes between 200 and 299
+
+    if (result.status < 200 || result.status > 299) {
+      throw new Error(result.statusText);
+    }
+
+    // check if 200 response has empty answer record
+    if (result.data.responseCode !== "NO_ERROR") {
+      throw new Error(result.data.responseCode);
+    }
+    return result.data;
   }
-  return result.data;
-};
+}
